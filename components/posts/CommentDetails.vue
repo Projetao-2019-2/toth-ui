62c36d50d1606c1d317ca71a13ac520fae57cd63
Mateus Valgueiro <mvt@cin.ufpe.br>
2019-10-16 19:18:08 -0300

    Add reply button

--- components/posts/CommentDetails.vue
+++ components/posts/CommentDetails.vue
@@ -1,18 +1,32 @@
 <template>
   <b-card class="comment-details-card" :sub-title="comment.user.nome + ' | ' + comment.user.email">
     <b-card-text>{{comment.texto}}</b-card-text>
+
+    <div v-on:click="click('voce clicou no reply')" class="reply-section">
+      Responder
+      <font-awesome-icon :icon="['fas', 'reply-all']"></font-awesome-icon>
+    </div>
   </b-card>
 </template>
 
 <script>
 export default {
   name: "CommentDetails",
-  props: ["comment"]
+  props: ["comment"],
+  methods: {
+    click(string) {
+      alert(string);
+    }
+  }
 };
 </script>
 
-<style scoped>
-.comment-details-card{
+<style>
+.comment-details-card {
   margin-top: 16px;
+}
+
+.reply-section {
+  text-align: right;
 }
 </style>
