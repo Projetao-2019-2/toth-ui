92462986a910adcb88c626bc1fc32915167b42c9
pedro <phls@cin.ufpe.br>
2019-10-20 12:06:01 -0300

    seach input and icon

--- pages/search/index.vue
+++ pages/search/index.vue
@@ -7,22 +7,40 @@
       <div class="display">
         <h1 id="brand">NORTUNI</h1>
       </div>
+      <div id="search" class="input-group mb-3">
+        <input id="input" class="form-control" type="text" placeholder="Curso - Universidade" />
+        <div class="input-group-append">
+          <span class="input-group-text" id="basic-addon2">
+            <div>
+              <img id="icon" src="~/assets/search-icon.png" alt="search" />
+            </div>
+          </span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <style>
-.display{
-    display: flex;
-    align-content: center;
-    justify-content: center;
+#icon {
+  width: 19px;
+  height: 19px;
 }
-#brand{
-    font-size: 100px;
+#search {
+  display: flex;
+  flex-direction: row;
 }
-#logo{
-    position: relative;
-    width: 35%;
-    height: 35%;
+.display {
+  display: flex;
+  align-content: center;
+  justify-content: center;
+}
+#brand {
+  font-size: 100px;
+}
+#logo {
+  position: relative;
+  width: 35%;
+  height: 35%;
 }
 </style>
