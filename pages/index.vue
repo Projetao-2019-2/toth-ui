86a27897d202ea6fe161108a98cd7848a8c50286
rosig <rglj2@cin.ufpe.br>
2019-10-26 16:22:24 -0300

    melhorias na tela de busca

--- pages/index.vue
+++ pages/index.vue
@@ -1,46 +1,60 @@
 <template>
-  <div class="container">
-    <div>
-      <div class="display">
-        <img id="logo" src="~/assets/compass1.png" alt="nortuni" />
-      </div>
-      <div class="display">
-        <h1 id="brand">NORTUNI</h1>
-      </div>
-      <div id="search" class="input-group mb-3">
-        <input id="input" class="form-control" type="text" placeholder="Curso - Universidade" />
-        <div class="input-group-append">
-          <span class="input-group-text" id="basic-addon2">
-            <div>
-              <img id="icon" src="~/assets/search-icon.png" alt="search" />
-            </div>
-          </span>
-        </div>
-      </div>
+  <div class="search-container">
+    <div class="logo-wrapper">
+      <img src="~/assets/compass1.png" alt="nortuni" width="200px" height="200px"/>
+      <span class="name-logo"> Nortuni </span>
+    </div>
+    <div class="input-search-wrapper">
+      <b-input-group>
+        <b-form-input v-model="searchText" placeholder="Curso - Universidade"></b-form-input>
+        <b-input-group-append>
+          <b-button variant="primary" class="btn-search" @click="doSearch(searchText)">
+            <font-awesome-icon :icon="['fas', 'search']"></font-awesome-icon>
+          </b-button>
+        </b-input-group-append>
+      </b-input-group>
     </div>
   </div>
 </template>
 
+<script>
+export default {
+  data () {
+    return {
+      searchText: ''
+    }
+  },
+  methods: {
+    doSearch (text) {
+      console.log('O texto da pesquisa foi: ' + text);
+    }
+  }
+}
+</script>
+
 <style>
-#icon {
-  width: 19px;
-  height: 19px;
-}
-#search {
-  display: flex;
-  flex-direction: row;
-}
-.display {
-  display: flex;
-  align-content: center;
-  justify-content: center;
-}
-#brand {
-  font-size: 100px;
-}
-#logo {
-  position: relative;
-  width: 35%;
-  height: 35%;
-}
+  .search-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    width: 100%;
+  }
+  .logo-wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+  .name-logo {
+    font-size: 6em;
+  }
+  .input-search-wrapper {
+    width: 500px;
+  }
+  .btn-search {
+    width: 80px;
+  }
 </style>
