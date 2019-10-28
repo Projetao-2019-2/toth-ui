2d24752c151710e573bad2245904e9286c8641a3
Pedro <phls@cin.ufpe.br>
2019-10-28 15:57:05 -0300

    search is the index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,46 @@
 <template>
   <div class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        toth-ui
-      </h1>
-      <h2 class="subtitle">
-        Toth front end app
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+      <div class="display">
+        <img id="logo" src="~/assets/compass1.png" alt="nortuni" />
+      </div>
+      <div class="display">
+        <h1 id="brand">NORTUNI</h1>
+      </div>
+      <div id="search" class="input-group mb-3">
+        <input id="input" class="form-control" type="text" placeholder="Curso - Universidade" />
+        <div class="input-group-append">
+          <span class="input-group-text" id="basic-addon2">
+            <div>
+              <img id="icon" src="~/assets/search-icon.png" alt="search" />
+            </div>
+          </span>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
-
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
+#icon {
+  width: 19px;
+  height: 19px;
+}
+#search {
   display: flex;
+  flex-direction: row;
+}
+.display {
+  display: flex;
+  align-content: center;
   justify-content: center;
-  align-items: center;
-  text-align: center;
 }
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
+#brand {
   font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
 }
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+#logo {
+  position: relative;
+  width: 35%;
+  height: 35%;
 }
 </style>
